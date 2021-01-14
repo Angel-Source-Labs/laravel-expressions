e142313deb77ee38f247f02446a7ad511482b808
Brion Finlay <brion.finlay@gmail.com>
2021-01-14 01:34:03 -0600

    Update class structure to parallel Illuminate\Database

--- src/MySqlGrammar.php
+++ src/MySqlGrammar.php
@@ -4,6 +4,8 @@
 namespace AngelSourceLabs\LaravelExpressions;
 
 
+use AngelSourceLabs\LaravelExpressions\Query\Expression\HasExpression;
+
 class MySqlGrammar extends \Illuminate\Database\Query\Grammars\MySqlGrammar
 {
     public function parameter($value)
